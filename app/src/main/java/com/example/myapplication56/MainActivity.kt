3c833aa5852fd5147db573593ecb82f4a2b12f56
drsmjt-56 <dorastisimanjuntak27@gmail.com>
2025-10-17 10:20:33 +0700

    feat: implement activity and intent navigation

---
+++ app/src/main/java/com/example/myapplication56/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.myapplication56
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.util.logging.Logger
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        Log.d("mainActivity", "Aplikasi Berhasil dijalankan")
+
+        val etUsername: EditText = findViewById(R.id.et_username)
+        val btnLogin: Button = findViewById(R.id.btn_login)
+
+        btnLogin.setOnClickListener {
+            val username = etUsername.text.toString()
+            val intent = Intent(this, HomeActivity::class.java)
+
+            intent.putExtra("EXTRA_USERNAME", username)
+            startActivity(intent)
+        }
+
+    }
+}
